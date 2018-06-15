d28e397c83122bd29f504e5403a9e31ade9f447f
Matheus Furquim <matheus.fdecamargo@gmail.com>
2018-06-14 22:48:00 -0300

    app

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../../hero';
-import { HEROES } from '../mock-heroes'
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,20 +11,21 @@ import { HEROES } from '../mock-heroes'
 export class HeroesComponent implements OnInit {
 
   selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+  heroes : Hero[];
+
+  constructor(private heroService : HeroService) { }
+
+  getHeroes() : void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
-  heroes = HEROES;
-
-  constructor() { }
-
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
-import { Hero } from '../hero';
+import { Hero } from './hero';
 
 export const HEROES : Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
- ];
+  { id: 11, name: 'Mr. Madeus'},
+  { id: 12, name: 'Mathios' },
+  { id: 13, name: 'Matheus' },
+  { id: 14, name: 'Tirfo' },
+  { id: 15, name: 'Theus' },
+  { id: 16, name: 'Flynn' },
+  { id: 17, name: 'Mth' },
+  { id: 18, name: 'Reis' },
+  { id: 19, name: 'Assumpção' },
+  { id: 20, name: 'lindo' }
+];
